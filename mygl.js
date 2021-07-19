c66a2abbc9865c9954cd64dd174535e6d6434ec7
s.cobb.2022 <sjcobb2003@gmail.com>
2021-07-19 18:06:40 +0800

    initial commit - optimised code

---
+++ mygl.js
@@ -0,0 +1,270 @@
+import {
+    ARRAY_BUFFER,
+    ELEMENT_ARRAY_BUFFER,
+    TRIANGLES,
+}                         from "./util/constants.mjs";
+import {mat4, quat, vec3} from "./matrix/gl-matrix.js";
+import {arr_diff}         from "./util/utilities.js";
+
+let cubeRotation = 0.0;
+
+import {Shader} from "./lib/Shader.js";
+
+import {CubeGeometry} from "./lib/Geometry.js";
+
+import {Mesh} from "./lib/Mesh.js";
+
+import Stats from "./stats/stats.module.js";
+
+main();
+
+function main() {
+
+    const canvas = document.getElementById('glCanvas');
+
+    let gl = canvas.getContext('webgl');
+
+    if (!gl) {
+        alert('Unable to initialize WebGL. Your browser or machine may not support it.');
+        return;
+    }
+
+    let stats = new Stats();
+    stats.showPanel(0);
+    document.body.appendChild(stats.dom);
+
+    const vs = document.getElementById('vertShader').innerText;
+
+    const fs = document.getElementById('fragShader').innerText;
+
+    const shader = new Shader(vs, fs, gl);
+
+    let myObjArr = [];
+
+    let myGeom = new CubeGeometry("aPosition", "aTextureCoord", "aNormal");
+
+    for (let i = 0; i < 1000; i++) {
+
+        let obj = new Mesh(myGeom);
+
+        obj.addUniform('uColor', [Math.random(), Math.random(), Math.random(), Math.random()]);
+
+        obj.position = [((Math.random() * 2) - 1) * 200, ((Math.random() * 2) - 1) * 200, ((Math.random() * 2) - 1) * 200];
+
+        obj.rotateRad(Math.random() * Math.PI * 2, Math.random() * Math.PI * 2, Math.random() * Math.PI * 2);
+
+        myObjArr.push(obj);
+    }
+
+    let then = 0;
+    let rq;
+
+    function render(now) {
+        now *= 0.001;  // convert to seconds
+        const deltaTime = now - then;
+        then            = now;
+        stats.begin();
+        draw(gl, shader, myObjArr, deltaTime);
+        stats.end();
+
+        rq = requestAnimationFrame(render);
+    }
+
+    rq = requestAnimationFrame(render);
+
+    window.stopRender = function(){
+        cancelAnimationFrame(rq);
+        rq = null;
+    }
+
+    window.startRender = function(){
+        rq = requestAnimationFrame(render);
+    }
+}
+
+function draw(gl, shader, objectArr, deltaTime) {
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clearDepth(1.0);
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
+
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    const fieldOfView = 45 * Math.PI / 180;   // in radians
+    const aspect      = gl.canvas.clientWidth / gl.canvas.clientHeight;
+    const zNear       = 0.1;
+    const zFar        = 1000.0;
+
+    const projectionMatrix = mat4.create();
+
+    mat4.perspective(projectionMatrix, fieldOfView, aspect, zNear, zFar);
+
+    const viewMatrix = mat4.create();
+
+    mat4.translate(viewMatrix, viewMatrix, [0.0, 0.0, -20.]);
+
+    mat4.rotate(viewMatrix, viewMatrix,
+                cubeRotation * 0.5,
+                [0, 0, 1]);
+
+    mat4.rotate(viewMatrix, viewMatrix, cubeRotation * .2, [0, 1, 0]);
+
+    for (let i = 0; i < objectArr.length; i++) {
+        const object = objectArr[i];
+
+        if (object.geom.needsBuffers === true) {
+            // console.log(object.geom);
+            object.geom.initBuffers(gl);
+            object.geom.needsBuffers = false;
+        }
+
+        if (object.needsModelUpdate === true) {
+            object.updateModelMatrix();
+            // console.log(object.uniforms);
+            // console.log('updateing model matrix')
+            object.needsModelUpdate = false;
+        }
+
+        //TODO add shader verification map maybe?   <-- not necessary rn but if slows down maybe
+
+        const object_attr_length = Object.keys(object.geom.attributes).length;
+
+        const comparison_obj = Object.assign({}, object.geom.attributes, shader.info.attr);
+
+        const comparison_obj_length = Object.keys(comparison_obj).length;
+
+        if (comparison_obj_length > object_attr_length) {
+
+            if (object.indexed === true) {
+
+                const initial_array = arr_diff(Object.keys(object.geom.attributes), Object.keys(shader.info.attr));
+
+                //only get undefined props
+                const final_array = initial_array.filter(x => object.geom.attributes[x] === undefined);
+
+                throw `Object does not have properties: ${final_array}`;
+
+            }
+            else {
+                throw `Object does not have properties: ${arr_diff(object.geom.attributes.keys(), shader.info.attr.keys())}`;
+            }
+
+        }
+
+        for (let key in object.geom.attributes) {
+
+            if (!object.geom.attributes.hasOwnProperty(key)) continue;
+
+            //if is a index buffer
+            if (object.geom.attributes[key].index === true) {
+
+                gl.bindBuffer(ELEMENT_ARRAY_BUFFER, object.geom.buffers[key]);
+
+            }
+
+            else {
+
+
+                const numComponents = object.geom.attributes[key].size;
+                const type          = gl.FLOAT;
+                const normalize     = false;
+                const stride        = 0;
+                const offset        = 0;
+
+                // console.log(object.buffers[key])
+
+                gl.bindBuffer(ARRAY_BUFFER, object.geom.buffers[key]);
+
+                gl.vertexAttribPointer(
+                    shader.info.attr[key],
+                    numComponents,
+                    type,
+                    normalize,
+                    stride,
+                    offset);
+
+                gl.enableVertexAttribArray(
+                    shader.info.attr[key]);
+            }
+
+        }
+
+        if (!(gl.getParameter(gl.CURRENT_PROGRAM) === shader.program)) {
+            gl.useProgram(shader.program);
+        }
+
+        const obj_uni    = object.uniforms;
+        const shader_uni = shader.info.uni;
+
+        for (let key in obj_uni) {
+
+            if (!obj_uni.hasOwnProperty(key)) continue;
+
+            if (!shader_uni[key]) {
+                console.warn(`shader does not have uniform ${key}, will not apply`);
+                continue;
+            }
+
+            if (shader_uni[key].data.length > obj_uni[key].length) {
+                throw new RangeError(`The uniform ${key} has data which is too short`);
+            }
+
+            try {
+                shader_uni[key].data.set(obj_uni[key], 0);
+            } catch {
+
+                throw new RangeError(`The uniform ${key} has a length that is too long`);
+
+            }
+
+            if (shader_uni[key].matrix === true) {
+
+                shader_uni[key].func.call(gl, shader_uni[key].location, false, shader_uni[key].data);
+
+            }
+            else {
+
+                shader_uni[key].func.call(gl, shader_uni[key].location, shader_uni[key].data);
+
+            }
+        }
+
+        gl.uniformMatrix4fv(
+            shader.info.uni['uProjectionMatrix'].location,
+            false,
+            projectionMatrix);
+
+        gl.uniformMatrix4fv(
+            shader.info.uni['uViewMatrix'].location,
+            false,
+            viewMatrix);
+
+        const normalMatrix = mat4.create();
+
+        mat4.invert(normalMatrix, object.model);
+        mat4.transpose(normalMatrix, normalMatrix);
+
+        gl.uniformMatrix4fv(shader.info.uni['uNormalMatrix'].location, false, normalMatrix);
+
+        if (object.geom.indexed === true) {
+
+            {
+                const vertexCount = object.geom.attributes['indices'].data.length;
+                const type        = gl.UNSIGNED_SHORT;
+                const offset      = 0;
+                gl.drawElements(TRIANGLES, vertexCount, type, offset);
+            }
+
+        }
+        else {
+
+            gl.drawArrays(TRIANGLES, 0, object.geom.attributes['aPosition'].data.length);
+
+        }
+    }
+
+    cubeRotation += deltaTime;
+
+}
+
---
+++ util/constants.mjs
@@ -0,0 +1,127 @@
+export const COLOR_BUFFER_BIT   = 16384;
+export const DEPTH_BUFFER_BIT   = 256;
+export const STENCIL_BUFFER_BIT = 1024;
+
+export const TEXTURE_2D       = 3553;
+export const TEXTURE_CUBE_MAP = 34067;
+export const TEXTURE_3D       = 32879;
+
+export const TEXTURE_MAG_FILTER = 10240;
+export const TEXTURE_MIN_FILTER = 10241;
+export const TEXTURE_WRAP_S     = 10242;
+export const TEXTURE_WRAP_T     = 10243;
+
+export const BYTE           = 5120;
+export const UNSIGNED_BYTE  = 5121;
+export const SHORT          = 5122;
+export const UNSIGNED_SHORT = 5123;
+export const INT            = 5124;
+export const UNSIGNED_INT   = 5125;
+export const FLOAT          = 5126;
+export const HALF_FLOAT_OES = 36193; //webgl 1.0 only
+
+export const FLOAT_VEC2 = 35664;
+export const FLOAT_VEC3 = 35665;
+export const FLOAT_VEC4 = 35666;
+export const INT_VEC2   = 35667;
+export const INT_VEC3   = 35668;
+export const INT_VEC4   = 35669;
+export const BOOL       = 35670;
+export const BOOL_VEC2  = 35671;
+export const BOOL_VEC3  = 35672;
+export const BOOL_VEC4  = 35673;
+export const FLOAT_MAT2 = 35674;
+export const FLOAT_MAT3 = 35675;
+export const FLOAT_MAT4 = 35676;
+
+export const SAMPLER_2D                = 35678;
+export const SAMPLER_3D                = 35679;
+export const SAMPLER_CUBE              = 35680;
+export const INT_SAMPLER_2D            = 36298;
+export const INT_SAMPLER_3D            = 36299;
+export const INT_SAMPLER_CUBE          = 36300;
+export const UNSIGNED_INT_SAMPLER_2D   = 36306;
+export const UNSIGNED_INT_SAMPLER_3D   = 36307;
+export const UNSIGNED_INT_SAMPLER_CUBE = 36308;
+
+export const DEPTH_COMPONENT         = 6402;
+export const ALPHA                   = 6406;
+export const RGB                     = 6407;
+export const RGBA                    = 6408;
+export const LUMINANCE               = 6409;
+export const LUMINANCE_ALPHA         = 6410;
+export const DEPTH_STENCIL           = 34041;
+export const UNSIGNED_INT_24_8_WEBGL = 34042;
+
+export const NEAREST                = 9728;
+export const LINEAR                 = 9729;
+export const NEAREST_MIPMAP_NEAREST = 9984;
+export const LINEAR_MIPMAP_NEAREST  = 9985;
+export const NEAREST_MIPMAP_LINEAR  = 9986;
+export const LINEAR_MIPMAP_LINEAR   = 9987;
+
+export const REPEAT          = 10497;
+export const CLAMP_TO_EDGE   = 33071;
+export const MIRRORED_REPEAT = 33648;
+
+export const ZERO                     = 0;
+export const ONE                      = 1;
+export const SRC_COLOR                = 768;
+export const ONE_MINUS_SRC_COLOR      = 769;
+export const SRC_ALPHA                = 770;
+export const ONE_MINUS_SRC_ALPHA      = 771;
+export const DST_ALPHA                = 772;
+export const ONE_MINUS_DST_ALPHA      = 773;
+export const DST_COLOR                = 774;
+export const ONE_MINUS_DST_COLOR      = 775;
+export const SRC_ALPHA_SATURATE       = 776;
+export const CONSTANT_COLOR           = 32769;
+export const ONE_MINUS_CONSTANT_COLOR = 32770;
+export const CONSTANT_ALPHA           = 32771;
+export const ONE_MINUS_CONSTANT_ALPHA = 32772;
+
+export const VERTEX_SHADER   = 35633;
+export const FRAGMENT_SHADER = 35632;
+
+export const FRONT          = 1028;
+export const BACK           = 1029;
+export const FRONT_AND_BACK = 1032;
+
+export const NEVER    = 512;
+export const LESS     = 513;
+export const EQUAL    = 514;
+export const LEQUAL   = 515;
+export const GREATER  = 516;
+export const NOTEQUAL = 517;
+export const GEQUAL   = 518;
+export const ALWAYS   = 519;
+
+export const KEEP      = 7680;
+export const REPLACE   = 7681;
+export const INCR      = 7682;
+export const DECR      = 7683;
+export const INCR_WRAP = 34055;
+export const DECR_WRAP = 34056;
+export const INVERT    = 5386;
+
+export const STREAM_DRAW  = 35040;
+export const STATIC_DRAW  = 35044;
+export const DYNAMIC_DRAW = 35048;
+
+export const ARRAY_BUFFER         = 34962;
+export const ELEMENT_ARRAY_BUFFER = 34963;
+
+export const POINTS         = 0;
+export const LINES          = 1;
+export const LINE_LOOP      = 2;
+export const LINE_STRIP     = 3;
+export const TRIANGLES      = 4;
+export const TRIANGLE_STRIP = 5;
+export const TRIANGLE_FAN   = 6;
+
+export const CW  = 2304;
+export const CCW = 2305;
+
+export const CULL_FACE  = 2884;
+export const DEPTH_TEST = 2929;
+export const BLEND      = 3042;
